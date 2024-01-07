1541d2f57e08684321a3672da642ead643e42d1a
Prashu9108 <prashu9108@gmail.com>
2024-01-07 20:08:24 +0530

    code

---
+++ JavaAssignment/src/com/assignment/Asgn1.java
@@ -0,0 +1,27 @@
+package com.assignment;
+
+public class Asgn1 {
+	public static void main (String[] args) {
+		
+		int number =45;
+		 
+	     
+		 
+	    if (number > 0) {
+	 
+	      System.out.println(number + " is positive.");
+	 
+	    } else if (number < 0) {
+	 
+	      System.out.println(number + " is negative.");
+	 
+	    } else {
+	 
+	      System.out.println(number + " is zero.");
+	 
+	    }
+	 
+	}
+	
+
+}
