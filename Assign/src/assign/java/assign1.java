71f19d0cd9961ce986b47bb87e0595066e42ff88
Prashu9108 <prashu9108@gmail.com>
2024-01-07 20:34:30 +0530

    code

---
+++ Assign/src/assign/java/assign1.java
@@ -0,0 +1,27 @@
+package assign.java;
+
+public class assign1 {
+
+	public static void main(String[] args) {
+		int number =-1;
+		 
+	     
+		 
+	    if (number > 0) {
+	 
+	      System.out.println(number + " is positive.");
+	 
+	    } else if (number < 0) {
+	 
+	      System.out.println(number + " is negative.");
+	 
+	    } else {
+
+			System.out.println(number + " is zero.");
+
+		}
+	 
+
+	}
+
+}
