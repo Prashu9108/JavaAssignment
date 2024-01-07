750b297bf488da511ac754fe405ef807dd4e2a2b
Prashu9108 <prashu9108@gmail.com>
2024-01-07 20:51:58 +0530

    assign code

---
+++ Assign/src/assign/java/assign3.java
@@ -0,0 +1,18 @@
+package assign.java;
+
+public class assign3 {
+
+	public static void main(String[] args) {
+		int i, num = 10, sum = 0;  
+		 
+		for(i = 1; i <= num; i++)  
+		{  
+		 
+		sum = sum + i;  
+		}  
+		 
+		System.out.println("Sum of First n Natural Numbers is = " + sum);  
+
+	}
+
+}
---
+++ Assign/src/assign/java/assign4.java
@@ -0,0 +1,17 @@
+package assign.java;
+
+public class assign4 {
+
+	public static void main(String[] args) {
+		int num1 = 50, num2 = 50;
+	     if (num1 == num2)
+	       System.out.println ("both are equal");
+	     else if (num1 > num2)
+	         System.out.println (num1 + " is greater");
+
+	     else
+	         System.out.println (num2 + " is greater");
+
+	}
+
+}
